f4254dcafcde3d6948b2bb6c742a136bf9fa18ba
Harry Krantz <harryk333@users.noreply.github.com>
2020-05-21 11:49:00 -0700

    Updated license info

--- writeAcpPlan.py
+++ writeAcpPlan.py
@@ -1,11 +1,24 @@
 # writeAcpPlan.py
 #
-# take a list of events and write an ACP plan to observe them
+# Take a list of events and write an ACP plan to observe them
 #
 # Harry Krantz
 # Steward Observatory
 # University of Arizona
-# May 2020
+# Copyright May 2020
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import datetime as dt
